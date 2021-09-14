500cd2b4b4ef5e29ffa7707bbaf180861a73b106
Esteban Arrangoiz <esteban.arrangoiz@gmail.com>
2021-09-14 01:03:54 -0400

    Agregue la separacion de los datos por columnas

--- scrapeSoccerTransfers.py
+++ scrapeSoccerTransfers.py
@@ -10,8 +10,13 @@ soup = BeautifulSoup(data.text, 'html.parser')
 #get data simply by looking for each <tr>
 data =[]
 for table in soup.find('table', {'class': 'Table'}):
-    for tr in table.find_all('tr', { 'class': 'Table__TR'}):
-        values = [td.text for td in tr.find_all('td')]
-        data.append(values)
+    for tr in table.find_all('tr'):
+        td = tr.find_all('td', {'class': 'Table__TD'})
+        if( len(td)>0 ):
+            fecha = td[0].text.strip()
+            jugador = td[1].text.strip()
+            equipoAnterior = td[2].find_all('span')[1].text.strip()
+            equipoNuevo = td[4].find_all('span')[1].text.strip()
+            print(fecha, jugador, equipoAnterior, equipoNuevo)
 
-print(data)
+        
